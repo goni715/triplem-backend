0977e9468e35c347807f623925521fd01d709052
goni715 <halesgoni715149123@gmail.com>
2025-08-05 11:24:53 +0600

    get-income-overview

--- src/app/modules/Order/Order.service.ts
+++ src/app/modules/Order/Order.service.ts
@@ -9,6 +9,7 @@ import mongoose, { Types } from "mongoose";
 import generateTransactionId from '../../utils/generateTransactionId';
 import Stripe from 'stripe';
 import config from '../../config';
+import isValidYearFormat from '../../utils/isValidateYearFormat';
 
 const stripe = new Stripe(config.stripe_secret_key as string);
 
@@ -553,8 +554,81 @@ const verifySessionService = async (sessionId: string) => {
 
 
 const getIncomeOverviewService = async (year: string) => {
-  
-  return "get Income Service";
+  if(!isValidYearFormat(year)){
+    throw new ApiError(400, "Invalid year, year should be in 'YYYY' format.")
+  }
+
+  const start = `${year}-01-01T00:00:00.000+00:00`;
+  const end = `${year}-12-31T00:00:00.000+00:00`;
+
+  const result = await OrderModel.aggregate([
+    {
+      $match: {
+        createdAt: { $gte: new Date(start), $lte: new Date(end) },
+        paymentStatus: "paid"
+      }
+    },
+    {
+      $group: {
+        _id: {
+          year: { $year: "$createdAt" },
+          month: { $month: "$createdAt" },
+        },
+        income: { $sum: "$totalPrice" },
+      },
+    },
+    {
+      $sort: {
+        "_id.year": 1,
+        "_id.month": 1,
+      },
+    },
+    {
+      $addFields: {
+        month: {
+          $arrayElemAt: [
+            [
+              "",
+              "Jan",
+              "Feb",
+              "Mar",
+              "Apr",
+              "May",
+              "Jun",
+              "Jul",
+              "Aug",
+              "Sep",
+              "Oct",
+              "Nov",
+              "Dec",
+            ],
+            "$_id.month",
+          ],
+        },
+      },
+    },
+    {
+      $project: {
+        _id:0
+      }
+    }
+  ])
+
+  // Fill in missing months
+  const allMonths = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
+
+  const filledData = allMonths.map((month) => {
+    const found = result?.find((item) => item.month === month);
+    return {
+      month,
+      income: found ? found.income : 0
+    };
+  });
+ 
+  return filledData;
 }
 
 export {
